e13ed514f6a87ff75a7dbab0f5acaaaa68c04546
3239987006@qq.com <3239987006@qq.com>
2018-12-04 15:36:04 +0800

    我的关注页更新

--- Makeup/src/pages/my-follows/my-follows.ts
+++ Makeup/src/pages/my-follows/my-follows.ts
@@ -15,11 +15,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyFollowsPage {
   isActive:boolean=true;
-  names=['Canna','Jon','Jack'];
-  follows=[{name:'昵称1',note_num:23,fan_num:3456},
-    {name:'昵称1',note_num:23,fan_num:3456},
-    {name:'昵称1',note_num:23,fan_num:3456}];
+  names=[{name:'Canna',img:"../../assets/images/img1.jpg"},
+         {name:'Jon',img:"../../assets/images/img2.jpg"},
+         {name:'Jack',img:"../../assets/images/img3.jpg"}];
+  follows=[{name:'梅子熟了',img:"../../assets/images/img4.jpg",note_num:20,fan_num:456},
+    {name:'雨夜',img:"../../assets/images/img5.jpg",note_num:23,fan_num:34},
+    {name:'听见你说',img:"../../assets/images/img6.jpg",note_num:12,fan_num:58}];
   
+  names2=[{name:'护肤话题',img:"../../assets/images/img27.jpg"},
+          {name:'健身话题',img:"../../assets/images/img28.jpg"},
+          {name:'香氛话题',img:"../../assets/images/img24.jpg"}];
+  follows2=[
+    {name:'穿搭话题',img:"../../assets/images/淘淘淘.jpg",note_num:265,fan_num:346},
+    {name:'美妆话题',img:"../../assets/images/img25.jpg",note_num:124,fan_num:1246}
+  ];
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
--- Makeup/src/pages/wrelease/wrelease.ts
+++ Makeup/src/pages/wrelease/wrelease.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { HomePage } from '../home/home';
+
+
 
 /**
  * Generated class for the WreleasePage page.
@@ -23,7 +24,7 @@ export class WreleasePage {
     console.log('ionViewDidLoad WreleasePage');
   }
   cancel(){
-    this.navCtrl.push(HomePage);
+    this.navCtrl.popToRoot();
   }
    
   Publish(){     
