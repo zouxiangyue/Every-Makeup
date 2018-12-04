cb70270d92b40708c152f04841445dfc092e2ab2
3239987006@qq.com <3239987006@qq.com>
2018-12-04 15:46:33 +0800

    完善

--- Makeup/src/pages/about/about.ts
+++ Makeup/src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SearchPage } from '../search/search';
 
 @Component({
   selector: 'page-about',
@@ -9,5 +10,8 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  search(){
+    this.navCtrl.push(SearchPage);
   }
 }
