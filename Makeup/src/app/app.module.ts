9c29db3feea7fd8a479b5e88807cb3b910d3ac39
Liyue1998 <1757759700@qq.com>
2018-12-11 16:21:40 +0800

    错误更改

--- Makeup/src/app/app.module.ts
+++ Makeup/src/app/app.module.ts
@@ -64,21 +64,11 @@ import { PaPage } from '../pages/pa/pa';
     PinglunPage,
     AitePage,
     SearchPage,
-<<<<<<< HEAD
-=======
-
->>>>>>> 87c2de3113f94f5b771ef15aa50798535950c5e4
     ManagePage,
     SecurityPage,
     XxsetPage,
     TysetPage,
-<<<<<<< HEAD
     PaPage
-=======
-
-    PaPage
-
->>>>>>> 87c2de3113f94f5b771ef15aa50798535950c5e4
   ],
   imports: [
     BrowserModule,
@@ -115,21 +105,11 @@ import { PaPage } from '../pages/pa/pa';
     PinglunPage,
     AitePage,
     SearchPage,
-<<<<<<< HEAD
-=======
-
->>>>>>> 87c2de3113f94f5b771ef15aa50798535950c5e4
     ManagePage,
     SecurityPage,
     XxsetPage,
     TysetPage,
-<<<<<<< HEAD
     PaPage
-=======
-
-    PaPage
-
->>>>>>> 87c2de3113f94f5b771ef15aa50798535950c5e4
   ],
   providers: [
     StatusBar,
