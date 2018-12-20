fb42ea0eb6945b8bd59a9803a213bd80e688087a
3239987006@qq.com <3239987006@qq.com>
2018-12-20 16:19:22 +0800

    文章详情页更新

--- Makeup_front/src/pages/pa/pa.ts
+++ Makeup_front/src/pages/pa/pa.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, ActionSheetController  } from 'ionic-angular';
+import { AppShare } from '../../app/app.share';
 /**
  * Generated class for the PaPage page.
  *
@@ -15,11 +15,49 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public appShare: AppShare, public actionSheetCtrl: ActionSheetController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PaPage');
   }
-
+  share(event) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: '分享',
+      buttons: [
+        {
+          text: 'QQ好友',
+          handler: () => {
+            this.appShare.qqShare(0)
+          }
+        },
+        {
+          text: 'QQ空间',
+          handler: () => {
+            this.appShare.qqShare(1)
+          }
+        },
+        {
+          text: '微信好友',
+          handler: () => {
+            this.appShare.wxShare(0)
+          }
+        },
+        {
+          text: '朋友圈',
+          handler: () => {
+            this.appShare.wxShare(1)
+          }
+        },
+        {
+          text: '取消',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }
