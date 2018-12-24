98d3d8680e74c16c2ea843811f38fb90305b3db7
zhangteng12 <928775004@qq.com>
2018-12-24 17:54:16 +0800

    粉丝页面

--- Makeup_front/src/pages/fensi/fensi.ts
+++ Makeup_front/src/pages/fensi/fensi.ts
@@ -7,7 +7,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'fensi.html',
 })
 export class FensiPage {
-
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -17,4 +16,15 @@ export class FensiPage {
   back(){
     this.navCtrl.pop();
   }
+  change(e){
+      if(e.target.value=='关注'){
+        e.target.value='互关';
+        e.target.style.backgroundColor='#C1B7B7';
+      }else if(e.target.value=='互关'){
+        e.target.value='关注';
+        e.target.style.backgroundColor='#E17579';
+      }
+  }
+
+  
 }
