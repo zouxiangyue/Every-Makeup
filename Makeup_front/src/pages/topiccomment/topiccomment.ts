97e9dec1743d2ea7b37d46f0da80490c5a780fa6
Liyue1998 <1757759700@qq.com>
2018-12-28 09:06:23 +0800

    话题“讨论”前端页面已完成，及其他地方稍作改动

---
+++ Makeup_front/src/pages/topiccomment/topiccomment.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-topiccomment',
+  templateUrl: 'topiccomment.html',
+})
+export class TopiccommentPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TopiccommentPage');
+  }
+
+  
+ send(){
+  
+ }
+
+}
